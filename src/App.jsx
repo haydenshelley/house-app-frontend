6fd07c6e3ee32b9857a214f10deb38b9e7d129ca
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-07 09:25:09 -0700

    add header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Content } from "./Content";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 function App() {
   return (
     <div>
+      <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
