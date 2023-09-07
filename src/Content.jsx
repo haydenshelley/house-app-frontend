236f955a260643114f0934d6905c6e32f569cfba
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-07 10:47:15 -0700

    update house on show page

--- src/Content.jsx
+++ src/Content.jsx
@@ -21,6 +21,23 @@ export function Content() {
     });
   };
 
+  const handleHouseUpdate = (id, params, successCallback) => {
+    axios
+      .patch(`http://localhost:3000/houses/${id}.json`, params)
+      .then((response) => {
+        setHouses(
+          houses.map((house) => {
+            if (house.id === response.data.id) {
+              return response.data;
+            } else {
+              return house;
+            }
+          })
+        );
+        successCallback();
+      });
+  };
+
   useEffect(handleHousesIndex, []);
 
   return (
@@ -28,7 +45,10 @@ export function Content() {
       <h1>House App</h1>
       <Routes>
         <Route path="/houses" element={<HousesIndex houses={houses} />} />
-        <Route path="/houses/:id" element={<HousesShow />} />
+        <Route
+          path="/houses/:id"
+          element={<HousesShow onHouseUpdate={handleHouseUpdate} />}
+        />
         <Route
           path="/houses/new"
           element={<HousesNew onHousesCreate={handleHousesCreate} />}
--- src/HousesShow.jsx
+++ src/HousesShow.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-export function HousesShow() {
+export function HousesShow(props) {
   const [house, setHouse] = useState({});
   const params = useParams();
 
@@ -14,6 +14,12 @@ export function HousesShow() {
       });
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onHouseUpdate(house.id, params, () => event.target.reset());
+  };
+
   useEffect(getHouseData, []);
 
   return (
@@ -22,6 +28,26 @@ export function HousesShow() {
       <h2>Bedrooms: {house.bedrooms}</h2>
       <h2>Bathrooms: {house.bathrooms}</h2>
       <h2>Square Feet: {house.sqft}</h2>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <div>
+          Address:{" "}
+          <input defaultValue={house.address} name="address" type="text" />
+        </div>
+        <div>
+          Square Feet:{" "}
+          <input defaultValue={house.sqft} name="sqft" type="text" />
+        </div>
+        <div>
+          Bedrooms:{" "}
+          <input defaultValue={house.bedrooms} name="bedrooms" type="text" />
+        </div>
+        <div>
+          Bathrooms:{" "}
+          <input defaultValue={house.bathrooms} name="bathrooms" type="text" />
+        </div>
+        <button type="submit">Update House</button>
+      </form>
     </div>
   );
 }
